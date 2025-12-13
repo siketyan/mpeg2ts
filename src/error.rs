5006ffe7ddbbd8a4588b2d219b2ca71286aa738a
Takeru Ohta <phjgt308@gmail.com>
2025-12-13 14:06:03 +0900

    chore: remove trackable dependency

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,3 @@
-use trackable::error::{ErrorKind as TrackableErrorKind, TrackableError};
-
-/// This crate specific `Error` type.
-#[derive(Debug, Clone, trackable::TrackableError)]
-pub struct Error(TrackableError<ErrorKind>);
-
 /// Possible error kinds.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 #[allow(missing_docs)]
@@ -12,4 +6,61 @@ pub enum ErrorKind {
     Unsupported,
     Other,
 }
-impl TrackableErrorKind for ErrorKind {}
+
+///
+/// This struct captures the error kind, reason, source location, and backtrace
+/// to provide comprehensive error diagnostics.
+pub struct Error {
+    /// The kind of error that occurred.
+    pub kind: ErrorKind,
+
+    /// The reason why the error occurred.
+    pub reason: String,
+
+    /// The source code location where the error was created.
+    pub location: &'static std::panic::Location<'static>,
+
+    /// A backtrace showing the call stack at the point of error.
+    ///
+    /// The backtrace is only captured if the `RUST_BACKTRACE` environment variable is set.
+    pub backtrace: std::backtrace::Backtrace,
+
+    /// The underlying IO error, if this error originated from an IO operation.
+    pub io_error: Option<std::io::Error>,
+}
+
+impl std::fmt::Debug for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{self}")
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}: {}", self.kind, self.reason)?;
+        write!(f, " (at {}:{})", self.location.file(), self.location.line())?;
+        if self.backtrace.status() == std::backtrace::BacktraceStatus::Captured {
+            write!(f, "\n\nBacktrace:\n{}", self.backtrace)?;
+        }
+        Ok(())
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        self.io_error.as_ref()
+    }
+}
+
+impl From<std::io::Error> for Error {
+    #[track_caller]
+    fn from(io_error: std::io::Error) -> Self {
+        Self {
+            kind: ErrorKind::Other,
+            reason: io_error.to_string(),
+            location: std::panic::Location::caller(),
+            backtrace: std::backtrace::Backtrace::capture(),
+            io_error: Some(io_error),
+        }
+    }
+}
